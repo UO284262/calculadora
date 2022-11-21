d0cd1fc74d9e0458a94e55773d6d076b4247b6d6
UO284262 <98962592+UO284262@users.noreply.github.com>
2022-11-21 18:51:07 +0100

    Add files via upload

---
+++ MasInfoNavegador.js
@@ -0,0 +1,12 @@
+document.write("Otros datos del navegador:\n");
+document.write("Versión: ");
+document.write(infoNavegador.version);
+document.write("\nPlataforma: ");
+document.write(infoNavegador.plataforma);
+document.write("\nVendedor: ");
+document.write(infoNavegador.vendedor);
+document.write("\nAgente: ");
+document.write(infoNavegador.agente);
+document.write("\nJava activo: ");
+document.write(infoNavegador.javaActivo);
+
---
+++ Parrafos.js
@@ -0,0 +1,4 @@
+document.write("<p>"+asignatura.curso+"</p>");
+document.write("<p>"+asignatura.estudiante+"</p>");
+document.write("<p>"+asignatura.email+"</p>");
+
